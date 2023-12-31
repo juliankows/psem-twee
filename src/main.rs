a5135340f2948eac38880109c73fec8e0b85b4c8
Julian K. <julikows+gh@gmail.com>
2023-12-31 11:08:54 +0100

    try with video-rs (not working)

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{
 
 use anyhow::Result;
 use itertools::Itertools;
+use video_rs::{Decoder, Encoder, EncoderSettings, Locator, Options};
 
 #[derive(Debug)]
 struct Block {
@@ -33,7 +34,7 @@ fn main() -> Result<()> {
 		println!("{}", l.text_content);
 		println!("linked to {:?}", l.links);
 	}
-	process_block(blocks);
+	process_blocks(blocks);
 	Ok(())
 }
 fn read_twee(rdr: impl Iterator<Item = String>) -> BTreeMap<String, BlockConfig> {
@@ -55,6 +56,14 @@ fn read_twee(rdr: impl Iterator<Item = String>) -> BTreeMap<String, BlockConfig>
 				.to_string();
 			let (links, content): (Vec<String>, Vec<String>) =
 				c.into_iter().partition(|x| x.starts_with("[["));
+			let links: Vec<String> = links
+				.into_iter()
+				.map(|x| {
+					x.trim_start_matches("[[")
+						.trim_end_matches("]]")
+						.to_string()
+				})
+				.collect();
 			(
 				t.clone(),
 				BlockConfig {
@@ -66,15 +75,49 @@ fn read_twee(rdr: impl Iterator<Item = String>) -> BTreeMap<String, BlockConfig>
 		})
 		.collect()
 }
-fn process_block(blocks: BTreeMap<String, BlockConfig>) {
+fn process_blocks(blocks: BTreeMap<String, BlockConfig>) -> Result<()> {
 	let paths: Vec<PathBuf> = blocks
 		.keys()
 		.map(|x| PathBuf::from(format!("{x}.mp4")))
 		.collect();
-	if paths.iter().any(|x| !x.exists()) {
-		paths
-			.iter()
-			.filter(|x| !x.exists())
-			.for_each(|x| println!("{} existiert nicht", x.display()));
+	//if paths.iter().any(|x| !x.exists()) {
+	//	paths
+	//		.iter()
+	//		.filter(|x| !x.exists())
+	//		.for_each(|x| println!("{} existiert nicht", x.display()));
+	//}
+	for p in paths {
+		if !p.exists() {
+			continue;
+		}
+		println!("transcoding {}", p.display());
+		let p2 = p.clone();
+		let basename = p2.file_stem().unwrap().to_str().unwrap();
+
+		let src: Locator = p.into();
+		let mut dec = Decoder::new(&src).unwrap();
+
+		let dest: Locator = PathBuf::from(format!("out/{}.mpd", basename)).into();
+		let options = Options::new_from_hashmap(&HashMap::from([
+			("seg_duration".to_string(), "1".to_string()),
+			(format!("b"), format!("500k")),
+		]));
+		let (width, height) = dec.size_out();
+		let settings = EncoderSettings::for_h264_custom(
+			width as usize,
+			height as usize,
+			video_rs::ffmpeg::format::Pixel::YUV420P,
+			Options::new_from_hashmap(&HashMap::from([
+				("preset".to_string(), "medium".to_string()),
+				("b".to_string(), format!("500k")),
+			])),
+		);
+		let mut enc = Encoder::new_with_options(&dest, settings, &options).unwrap();
+
+		for fr in dec.decode_raw_iter() {
+			enc.encode_raw(fr?)?;
+		}
+		enc.finish()?;
 	}
+	Ok(())
 }
